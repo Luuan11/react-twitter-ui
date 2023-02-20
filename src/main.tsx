0331a950bcf2dc5d04c16178e5eb7b828bff6bfc
Luan Fernando <luancraft55@gmail.com>
2023-02-20 18:09:20 -0300

    :sparkles: added twitter-logo and sidebar progress

--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,26 @@ import './global.css';
 
 import { Tweet } from './components/Tweet';
 
+import twitterLogo from './assets/twitter-logo.svg'
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <div className="layout">
       <aside className="sidebar">
-        <img src="" alt="logo twitter" />
+        <img src={twitterLogo} className="logo" alt="logo twitter" />
 
-        <nav>
-
+        <nav className="sidebar-nav"> 
+          <a href="">Home</a>
+          <a href="">Explore</a>
+          <a href="">Notifications</a>
+          <a href="">Messages</a>
+          <a href="">Bookmarks</a>
+          <a href="">Twitter Blue</a>
+          <a href="">Profile</a>
+          <a href="">More</a>
         </nav>
 
-        <button>Tweet</button>
+        <button className="sidebar-btn">Tweet</button>
       </aside>
       <div className="content">
         
