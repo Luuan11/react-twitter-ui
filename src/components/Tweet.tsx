c56b7947283534807d507fa28b051c37416ad9b9
Luan Fernando <luancraft55@gmail.com>
2023-02-20 17:49:19 -0300

    :sparkles: added font roboto and start the sidebar progress

---
+++ src/components/Tweet.tsx
@@ -0,0 +1,11 @@
+interface TweetProps {
+    user: string
+    children: string
+    likes ?: number
+}
+
+export function Tweet() {
+    return(
+        <div>Tweet</div>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './global.css';
+
+import { Tweet } from './components/Tweet';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <div className="layout">
+      <aside className="sidebar">
+        <img src="" alt="logo twitter" />
+
+        <nav>
+
+        </nav>
+
+        <button>Tweet</button>
+      </aside>
+      <div className="content">
+        
+      </div>
+    </div>
   </React.StrictMode>,
 )
