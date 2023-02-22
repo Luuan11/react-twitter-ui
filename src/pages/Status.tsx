0f1f37ccb52b9d4003ff411444522a657f7f5579
Luan Fernando <luancraft55@gmail.com>
2023-02-22 20:56:53 -0300

    :sparkles: feat: added preventing defaults on the form and useState on the const

--- src/pages/Status.tsx
+++ src/pages/Status.tsx
@@ -1,16 +1,27 @@
+import { FormEvent, useState } from "react"
 import { Header } from "../components/Header"
 import { Separator } from "../components/Separator"
 import { Tweet } from "../components/Tweet"
 
-const answers = [
-    'Concordo',
-    'Faz sentido',
-    'Verdade'
-]
-
 import './Status.css'
 
 export function Status(){
+    const [newAnswer, setNewAnswer] = useState('')
+    const [answers, setAnswers] = useState([
+        'Concordo',
+        'Faz sentido',
+        'Verdade',
+    ])
+
+    function createNewAnswer(event: FormEvent){
+        event.preventDefault()
+
+        setAnswers([...answers, newAnswer])
+
+        //deletando o value do textarea
+        setNewAnswer('')
+    }
+
     return(
         <main className="status">
 
@@ -20,10 +31,16 @@ export function Status(){
 
             <Separator />
 
-            <form className="answer-tweet-form">
+            <form onSubmit={createNewAnswer} className="answer-tweet-form">
                 <label htmlFor="tweet">
                     <img src="https://pbs.twimg.com/profile_images/1456733134909841419/nWGCp-SS_400x400.jpg" alt="Luan Fernando" />
-                    <textarea id="tweet" placeholder="Tweet your answer" ></textarea>
+                    <textarea 
+                    id="tweet" 
+                    placeholder="Tweet your answer"
+                    onChange={(event) =>[
+                        setNewAnswer(event.target.value)
+                    ]}
+                     />
                 </label>
 
                 <button type="submit">Answer</button>
--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -1,25 +1,44 @@
+import { FormEvent, useState } from "react";
 import { Header } from "../components/Header";
 import { Separator } from "../components/Separator";
 import { Tweet } from "../components/Tweet";
 
 import './Timeline.css';
 
-const tweets = [
-    'Meu primeiro tweet',
-    'Teste',
-    'Deu certo'
-]
+let newTweet = ''
 
 export function Timeline(){
+    const [newTweet, setNewTweet] = useState('')
+    const [tweets, setTweets] = useState([
+        'Meu primeiro tweet',
+        'Teste',
+        'Deu certo!!'
+    ])
+
+    function createNewTweet(event: FormEvent){
+        event.preventDefault()
+
+        setTweets([...tweets, newTweet])
+
+        //deletando o value do textarea
+        setNewTweet('')
+    }
+
     return(
         <main className="timeline">
 
             <Header title='Home' />
 
-            <form className="new-tweet-form">
+            <form onSubmit={createNewTweet} className="new-tweet-form">
                 <label htmlFor="tweet">
                 <img src="https://pbs.twimg.com/profile_images/1456733134909841419/nWGCp-SS_400x400.jpg" alt="Luan Fernando" />
-                <textarea id="tweet" placeholder="What's happening? "></textarea>
+                <textarea 
+                    id="tweet" 
+                    placeholder="What's happening? "
+                    value={newTweet}
+                    onChange={(event) => {
+                        setNewTweet(event.target.value);
+                    }}/>
                 </label>
 
                 <button type="submit">Tweet</button>
